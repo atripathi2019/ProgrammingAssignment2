05cbfde3c2611f10e4d13aeec120deace29b8ecc
Arushi Tripathi <47637668+atripathi2019@users.noreply.github.com>
2019-05-22 16:51:01 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve functions calculates and stores inverse of a matrix. 
+## The inverse of the matrix is stored till the makeCacheMatrix is called again.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes matrix object as an argument and creates functions for the matrix.
+## This output of makeCacheMatrix is a list of functions:
+## set the matrix, get the matrix, setsolve (set the inverse), getsolve(get the already cached inverse)
+## set function is not practically used by cacheSolve but it is essential to reset the values in makeCacheMatrix
+## when new matrix comes in
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function takes makeCacheMatrix output as an argument. It then calls inverse value from makeCacheMatrix.
+## After that it checks if the inverse variable returned is null or has a value. 
+## if inverse variable not null, cacheSolve is getting the already stored value from makeCacheMatrix
+## otherwise, cacheSolve calculates the inverse of the matrix (which was passed in makeCacheMatrix as arugument)
+## and returns the inverse value to makeCacheMatrix to store for future.
+## For the first time when cacheSolve is called, cacheSolve will always calcualte the inverse of the matrix. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
